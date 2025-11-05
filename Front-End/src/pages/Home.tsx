3ecaa1945f0ec2ff7bdb22575fbe5622ddf13e97
Allan Victor <allansf29@gmail.com>
2025-11-04 21:02:18 -0300

    Separando o array de produtos.

--- Front-End/src/pages/Home.tsx
+++ Front-End/src/pages/Home.tsx
@@ -4,53 +4,7 @@ import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import type { Product } from "../types/type";
-
-const featuredProducts: Product[] = [
-  {
-    id: "p1",
-    title: "Jaqueta Urbana Masculina",
-    price: "R$ 249,90",
-    image:
-      "https://images.unsplash.com/photo-1745196587689-226859e4b32a?auto=format&fit=crop&q=80&w=688",
-    badge: "Novo",
-  },
-  {
-    id: "p2",
-    title: "Tênis Performance",
-    price: "R$ 399,00",
-    image:
-      "https://images.unsplash.com/photo-1760164141999-d000613107ba?auto=format&fit=crop&q=80&w=735",
-  },
-  {
-    id: "p3",
-    title: "Moletom Confort Fit",
-    price: "R$ 179,90",
-    image:
-      "https://images.unsplash.com/photo-1760126130338-4e6c9043ee2d?auto=format&fit=crop&q=80&w=764",
-    badge: "Promo",
-  },
-  {
-    id: "p4",
-    title: "Óculos Minimal",
-    price: "R$ 129,00",
-    image:
-      "https://images.unsplash.com/photo-1760476426758-7be6980cdfe1?auto=format&fit=crop&q=80&w=1057",
-  },
-    {
-    id: "p4",
-    title: "Óculos Minimal",
-    price: "R$ 129,00",
-    image:
-      "https://images.unsplash.com/photo-1760476426758-7be6980cdfe1?auto=format&fit=crop&q=80&w=1057",
-  },
-    {
-    id: "p4",
-    title: "Óculos Minimal",
-    price: "R$ 129,00",
-    image:
-      "https://images.unsplash.com/photo-1760476426758-7be6980cdfe1?auto=format&fit=crop&q=80&w=1057",
-  },
-];
+import { featuredProducts } from "../data/featuredProducts";
 
 const Home: FC = () => {
   const handleAddToCart = (product: Product) => {
