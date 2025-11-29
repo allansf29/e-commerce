f8526d0da6cc6ea43593378af529bb9b64e5999c
Allan Victor <111008768+allansf29@users.noreply.github.com>
2025-11-28 21:25:17 -0300

    Colocando cores do Dark Theme em alguns componentes que faltaram.

--- Front-End/src/pages/Home.tsx
+++ Front-End/src/pages/Home.tsx
@@ -168,7 +168,7 @@ const Home: FC = () => {
       <section className="container mx-auto px-6 lg:px-8 mt-10 pb-20 relative">
         <div className="flex items-center justify-between mb-8">
           <h2 className="text-2xl font-semibold tracking-tight">Produtos em Destaque</h2>
-          <Link to="/products" className="text-sm text-gray-600 dark:text-gray-300 hover:text-primary transition dark:text-secondary">
+          <Link to="/products" className="text-sm text-gray-600 dark:text-gray-300 hover:text-primary transition dark:text-white hover:dark:text-secondary">
             Ver todos
           </Link>
         </div>
