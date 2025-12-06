9e54eccaca0aca6fd2ac3b8b8cbc2d884d57db64
Allan Victor <111008768+allansf29@users.noreply.github.com>
2025-12-05 21:24:16 -0300

    Add ScrollToTop component to App

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routers";
 import Navbar from './components/Navbar'
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <main className="">
         <Navbar />
         <AppRoutes />
